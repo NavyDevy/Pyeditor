17ec516ec3892a2301858ebbed2441844b2cf826
NavyDevy <135717350+NavyDevy@users.noreply.github.com>
2023-06-12 08:22:26 +0300

    save load system
    yaay

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def quit (ev):
   root.destroy()
 def loadfile (ev):
   fn = tkfiledialog.Open (root.filetypes=[("*")]).show()
+  texetbox.delete ("1.0","end")
+  textbox.insert ("1.0",open(fn,"rt").read())
 root = TK()
 panelframe = Frame (root,height = 40, bg="black")
 textframe = Frame (root, width = 350, height = 300, bg = "dark-gray")
@@ -17,8 +19,11 @@ textbox ['yscrollcommand'] = scrollbar.set
 textbox.pack(side="left", fill="both", expand=1)
 scrollbar.pack(side="right",fill="y")
 textbox = text(textframe, font = "Sans Serif 12", wrap = "word")
+#buttons
 Save = Button(root,text = "Save").pack(side = "up-left", width = 30, height = 10)
-quitb = Button(root,text = "Quit").pack(side = "up-right", width 20, height = 10)
-Save.bind("<button-1>")savefile
-quitb.bind("<button-1>")quit
+quitb = Button(root,text = "Quit").pack(side = "up-right", width = 20, height = 10)
+load = Button(root,text = "load").pack(side = "up",width = 20, height = 10)
+load.bind("<nutton-1>"),loadfile
+Save.bind("<button-1>"),savefile
+quitb.bind("<button-1>"),quit
 root.mainloop()
